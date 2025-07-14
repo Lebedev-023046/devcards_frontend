0f8c33a862d49326cf2f014d7094866544e5e7be
lebedev-023046 <lebedev.023046@gmail.com>
2025-07-14 18:37:35 +0300

    refactor: delete unused symbol

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,15 +1,14 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { QueryClientProvider } from '@tanstack/react-query';
-import './index.css';
 import { queryClient } from '../shared/api/query-client.ts';
 import { AppRouter } from './AppRouter.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<AppRouter />
 		</QueryClientProvider>{' '}
-		d
 	</StrictMode>,
 );
