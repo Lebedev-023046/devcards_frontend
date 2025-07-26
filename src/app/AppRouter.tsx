4ea975d132bb8b7dbc397577d5f6ad8098a4b6b8
lebedev-023046 <lebedev.023046@gmail.com>
2025-07-27 01:26:40 +0300

    feat: add toasts

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,7 +1,9 @@
+import { useTheme } from '@/shared/styles/useTheme';
 import { AuthLayout } from '@/shared/ui/Layout/AuthLayout';
 import { BaseLayout } from '@/shared/ui/Layout/BaseLayout';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 const LandingPage = lazy(() => import('@/pages/LandingPage'));
 const SignInPage = lazy(() => import('@/pages/SignInPage'));
@@ -16,29 +18,42 @@ const RequireAuth = lazy(
 );
 
 export function AppRouter() {
+	const { theme } = useTheme();
+
 	return (
-		<BrowserRouter>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Routes>
-					<Route element={<AuthLayout />}>
-						<Route path='/signin' element={<SignInPage />} />
-						<Route path='/signup' element={<SignUpPage />} />
-					</Route>
-
-					<Route element={<BaseLayout />}>
-						<Route path='/' element={<LandingPage />} />
-						<Route element={<RequireAuth />}>
-							<Route path='/decks' element={<DecksPage />} />
-							<Route path='/decks/:deckId' element={<DeckPage />} />
-							<Route path='/decks/:deckId/cards' element={<CardsPage />} />
-							<Route path='/profile' element={<ProfilePage />} />
+		<>
+			<ToastContainer
+				position='top-center'
+				autoClose={3000}
+				hideProgressBar={false}
+				newestOnTop
+				closeOnClick
+				pauseOnHover
+				theme={theme}
+			/>
+			<BrowserRouter>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Routes>
+						<Route element={<AuthLayout />}>
+							<Route path='/signin' element={<SignInPage />} />
+							<Route path='/signup' element={<SignUpPage />} />
 						</Route>
-					</Route>
 
-					{/* not found */}
-					<Route path='*' element={<Navigate to='/' replace />} />
-				</Routes>
-			</Suspense>
-		</BrowserRouter>
+						<Route element={<BaseLayout />}>
+							<Route path='/' element={<LandingPage />} />
+							<Route element={<RequireAuth />}>
+								<Route path='/decks' element={<DecksPage />} />
+								<Route path='/decks/:deckId' element={<DeckPage />} />
+								<Route path='/decks/:deckId/cards' element={<CardsPage />} />
+								<Route path='/profile' element={<ProfilePage />} />
+							</Route>
+						</Route>
+
+						{/* not found */}
+						<Route path='*' element={<Navigate to='/' replace />} />
+					</Routes>
+				</Suspense>
+			</BrowserRouter>
+		</>
 	);
 }
