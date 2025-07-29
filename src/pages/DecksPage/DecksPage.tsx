b61e12c0d18a3c7ecc94528c5c0d64931d14695d
lebedev-023046 <lebedev.023046@gmail.com>
2025-07-29 14:37:54 +0300

    feat: add basic CreateDeck page

--- src/pages/DecksPage/DecksPage.tsx
+++ src/pages/DecksPage/DecksPage.tsx
@@ -1,5 +1,30 @@
+import { ROUTES } from '@/shared/routes';
+import { Button } from '@/shared/ui/Button';
+import { SquarePen } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import styles from './DecksPage.module.css';
+
 function DecksPage() {
-	return <div>DecksPage</div>;
+	const navigate = useNavigate();
+
+	return (
+		<section className={styles.deckPageWrapper}>
+			<div className={styles.sectionHeader}>
+				<h2>Публичные колоды</h2>
+				<div className={styles.sectionControls}>
+					<Button
+						variant='secondary'
+						startIcon={<SquarePen />}
+						onClick={() =>
+							navigate(ROUTES.CREATE_DECK(), { state: { isPrivate: false } })
+						}
+					>
+						Создать колоду
+					</Button>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default DecksPage;
