3f248609f987d233248b84c2b50d0aa915f3e73b
lebedev-023046 <lebedev.023046@gmail.com>
2025-08-06 00:25:24 +0300

    feat: separate DeckCard component and deletion logic & view

---
+++ src/features/deck/ui/DeckCard/useDeleteDeckView.tsx
@@ -0,0 +1,51 @@
+import { ConfirmDialog } from '@/shared/ui/сonfirmDialog';
+import { IconButton, useDisclosure } from '@chakra-ui/react';
+import { Trash } from 'lucide-react';
+import { useState } from 'react';
+import { useDeleteDeck } from '../../hooks/useDeleteDeck';
+
+export const useDeleteDeckView = () => {
+	const { open, onOpen, onClose } = useDisclosure();
+
+	const { mutate: removeDeck, isPending } = useDeleteDeck();
+	const [targetDeckId, setTargetDeckId] = useState<string | null>(null);
+
+	const handleDeleteDeck = async () => {
+		if (targetDeckId) {
+			removeDeck(targetDeckId);
+			console.log({ targetDeckId });
+		}
+		onClose();
+	};
+
+	const renderRemoveIcon = (deckId: string) => (
+		<IconButton aria-label='Remove deck' unstyled disabled={isPending}>
+			<Trash
+				cursor='pointer'
+				size='1.5rem'
+				onClick={() => {
+					setTargetDeckId(deckId);
+					onOpen();
+				}}
+			/>
+		</IconButton>
+	);
+
+	const renderConfirmDeckDeletionDialog = () => (
+		<ConfirmDialog
+			isOpen={open}
+			onClose={onClose}
+			onConfirm={handleDeleteDeck}
+			title='Удалить колоду?'
+			description='Вы уверены, что хотите удалить эту колоду? Это действие нельзя отменить.'
+			confirmText='Удалить'
+			cancelText='Отмена'
+			isLoading={isPending}
+		/>
+	);
+
+	return {
+		renderConfirmDeckDeletionDialog,
+		renderRemoveIcon,
+	};
+};
