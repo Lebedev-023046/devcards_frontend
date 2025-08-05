bf9828ea3aff5a4baf29d65509fce29f6628122c
lebedev-023046 <lebedev.023046@gmail.com>
2025-08-06 00:22:17 +0300

    refactor: update tags field with infinite scroll in select

--- src/entities/tag/api.ts
+++ src/entities/tag/api.ts
@@ -1,18 +1,32 @@
 import { ENDPOINTS } from '@/shared/api/endpoints';
 import type { ApiResponse } from '@/shared/api/types';
 import { api } from '@/shared/lib/api';
+import { infiniteQueryOptions } from '@tanstack/react-query';
 import type { TagResponse } from './model';
 
 export const tagsApi = {
-	async getAllTags({
-		page = 1,
-		limit = 10,
-		search = '',
-	}): Promise<ApiResponse<TagResponse>> {
-		const data = await api.get<ApiResponse<TagResponse>>(
-			ENDPOINTS.tags.getAll({ page, limit, search }),
-		);
+	getAllTags: ({ page = 1, limit = 10 }) =>
+		api
+			.get<ApiResponse<TagResponse>>(ENDPOINTS.tags.getAll({ page, limit }))
+			.then(res => res.data),
 
-		return data.data;
+	getAllTagsInfinityQueryOptions: ({ page = 1, limit = 10 }) => {
+		return infiniteQueryOptions({
+			queryKey: ['allTags', page, limit],
+			queryFn: meta => tagsApi.getAllTags({ page: meta.pageParam, limit: 10 }),
+			initialPageParam: 1,
+			getNextPageParam: lastPageResponse => {
+				if (lastPageResponse.data.lastPage > lastPageResponse.data.page)
+					return lastPageResponse.data.page + 1;
+				return undefined;
+			},
+			select: res =>
+				res.pages
+					.flatMap(page => page.data.data)
+					.map(tag => ({
+						label: tag.name,
+						value: tag.id,
+					})),
+		});
 	},
 };
--- src/entities/tag/hooks/useAllTags.tsx
+++ src/entities/tag/hooks/useAllTags.tsx
@@ -3,21 +3,7 @@ import { tagsApi } from '../api';
 
 export const useAllTags = (page: number = 1, limit: number = 10) => {
 	const { data, isLoading, error, fetchNextPage } = useInfiniteQuery({
-		queryKey: ['allTags', page, limit],
-		queryFn: meta => tagsApi.getAllTags({ page: meta.pageParam, limit: 10 }),
-		initialPageParam: 1,
-		getNextPageParam: lastPageResponse => {
-			if (lastPageResponse.data.lastPage > lastPageResponse.data.page)
-				return lastPageResponse.data.page + 1;
-			return undefined;
-		},
-		select: res =>
-			res.pages
-				.flatMap(page => page.data.data)
-				.map(tag => ({
-					label: tag.name,
-					value: tag.id,
-				})),
+		...tagsApi.getAllTagsInfinityQueryOptions({ page, limit }),
 	});
 
 	return { data, isLoading, error, fetchNextPage };
