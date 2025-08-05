bbc7a6f39be7c925a31d94282fa98aba7e3cce42
lebedev-023046 <lebedev.023046@gmail.com>
2025-08-05 17:44:32 +0300

    refactor: update Deck and Decks pages

--- src/pages/DeckPage/DeckPage.tsx
+++ src/pages/DeckPage/DeckPage.tsx
@@ -1,5 +1,47 @@
+import { useGetDeckById } from '@/features/deck/hooks/useGetDeckById';
+import {
+	Box,
+	Container,
+	Flex,
+	Heading,
+	Span,
+	Stack,
+	Text,
+} from '@chakra-ui/react';
+
 function DeckPage() {
-	return <div>DeckPage</div>;
+	const { data: deckInfo, error, isLoading } = useGetDeckById();
+
+	if (isLoading) return <div>Загрузка...</div>;
+	if (error) return <div>Ошибка загрузки</div>;
+	if (!deckInfo) return <div>Колода не найдена</div>;
+
+	return (
+		<Container p={0} m={0}>
+			<Flex alignItems={'flex-start'}>
+				<Stack gap={2}>
+					<Heading as={'h1'} fontSize={'3xl'}>
+						{deckInfo.title}
+					</Heading>
+					<Text fontSize={'xl'}>
+						<Span fontStyle={'italic'}>
+							{deckInfo.isPublic ? 'Публичная' : 'Приватная'}{' '}
+						</Span>
+						колода
+					</Text>
+				</Stack>
+
+				<Stack ml={'auto'} gap={2}>
+					<Text fontSize={'xl'} fontWeight={500}>
+						{deckInfo.cards.length} карточек
+					</Text>
+					<Box></Box>
+				</Stack>
+
+				{/* {deckInfo.description && <Text>{deckInfo.description}</Text>} */}
+			</Flex>
+		</Container>
+	);
 }
 
 export default DeckPage;
--- src/pages/DecksPage/DecksPage.tsx
+++ src/pages/DecksPage/DecksPage.tsx
@@ -1,15 +1,22 @@
+import { useGetPublicDecks } from '@/features/deck/hooks/useGetPublicDecks';
+import { DeckCard } from '@/features/deck/ui/DeckCard';
 import { ROUTES } from '@/shared/routes';
-import { Button, Heading } from '@chakra-ui/react';
+import { Box, Button, Heading, Wrap } from '@chakra-ui/react';
+
 import { useNavigate } from 'react-router-dom';
 import styles from './DecksPage.module.css';
 
 function DecksPage() {
 	const navigate = useNavigate();
 
+	const { data: publicDecks } = useGetPublicDecks();
+
 	return (
-		<section className={styles.deckPageWrapper}>
+		<Box height={'100%'}>
 			<div className={styles.sectionHeader}>
-				<Heading color='text-contrast'>Публичные колоды</Heading>
+				<Heading as={'h1'} fontSize={'3xl'} color='text-contrast'>
+					Публичные колоды
+				</Heading>
 				<div className={styles.sectionControls}>
 					<Button
 						bg={'button-primary-gradient'}
@@ -19,7 +26,19 @@ function DecksPage() {
 					</Button>
 				</div>
 			</div>
-		</section>
+			<Wrap gap={4} my={8}>
+				{publicDecks?.data.items.map(deck => (
+					<DeckCard
+						key={deck.id}
+						deckId={deck.id}
+						title={deck.title}
+						description={deck.description}
+						totalCards={deck.totalCards ?? 0}
+						tagNames={deck.deckTags.map(tagInfo => tagInfo.tag.name)}
+					/>
+				))}
+			</Wrap>
+		</Box>
 	);
 }
 
